0060970583a07c9f8fb0a103fc2312810ccd51ac
Dima <D130188@yandex.ru>
2023-07-11 00:19:50 +0400

    cors middleware

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { HTTP_STATUS_INTERNAL_SERVER_ERROR } = require('node:http2').constants;
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -22,13 +22,39 @@ mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
   console.log(err.message);
 });
 
-app.use(cors({
-  origin: [
-    'https://mesto.full.nomoredomains.work',
-    'http://mesto.full.nomoredomains.work',
-  ],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: [
+//     'https://mesto.full.nomoredomains.work',
+//     'http://mesto.full.nomoredomains.work',
+//   ],
+//   credentials: true,
+// }));
+const allowedCors = [
+  'https://mesto.full.nomoredomains.work',
+  'http://mesto.full.nomoredomains.work',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
 
 app.use(express.json());
 
